84436e7740b1a3e449cdc7c6ddbac221a65f19ce
Muzikanto <schiriy_maxim@icloud.com>
2024-08-24 12:37:16 +0300

    fix

--- docs/assets/index-BWJr8Iy6.js
+++ docs/assets/index-BWJr8Iy6.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-BXBeBCy7.js";import"./index-Bp6dStbm.js";/**
+import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-Dy9lBklI.js";import"./index-9kSELOi1.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
